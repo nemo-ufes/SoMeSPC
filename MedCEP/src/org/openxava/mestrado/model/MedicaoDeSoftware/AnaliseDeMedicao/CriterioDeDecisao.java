5e0b01900214eaa004451ebb3368aa3df25e51a3
Vinícius Fonseca <vinnysoft23@gmail.com>
2015-03-29 17:01:06 -0300

    Inclusão do cabeçalho da LGPL v3.

--- MedCEP/src/org/openxava/mestrado/model/MedicaoDeSoftware/AnaliseDeMedicao/CriterioDeDecisao.java
+++ MedCEP/src/org/openxava/mestrado/model/MedicaoDeSoftware/AnaliseDeMedicao/CriterioDeDecisao.java
@@ -1,17 +1,31 @@
+/*
+    MedCEP - A powerful tool for measure
+    
+    Copyright (C) 2013 Ciro Xavier Maretto
+    Copyright (C) 2015 Henrique Néspoli Castro, Vinícius Soares Fonseca                          
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/lgpl.html>.    
+ */
+
 package org.openxava.mestrado.model.MedicaoDeSoftware.AnaliseDeMedicao;
 
 import java.util.*;
+
 import javax.persistence.*;
 
 import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
-import org.openxava.mestrado.model.ComportamentoDeProcessosDeSoftware.*;
-import org.openxava.mestrado.model.MedicaoDeSoftware.Medicao.*;
-import org.openxava.mestrado.model.MedicaoDeSoftware.PlanejamentoDaMedicao.DefinicaoOperacional.*;
-import org.openxava.mestrado.model.MedicaoDeSoftware.PlanejamentoDaMedicao.EntidadeMensuravel.*;
-import org.openxava.mestrado.model.MedicaoDeSoftware.PlanejamentoDaMedicao.PlanoDeMedicao.*;
-import org.openxava.mestrado.model.OrganizacaoDeSoftware.*;
-import org.openxava.mestrado.model.ProcessoDeSoftware.*;
 
 //@Entity
 /*@Views({
