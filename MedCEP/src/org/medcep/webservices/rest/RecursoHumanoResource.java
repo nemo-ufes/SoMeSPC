0443b651b7dd3616a04c11f18fcbe16bd733534f
Vinícius Fonseca <vinnysoft23@gmail.com>
2015-04-05 22:14:25 -0300

    Correção do webservice para verificar transação ativa do entity manager e verificar ConstraintViolationException durante o HTTP POST.

--- MedCEP/src/org/medcep/webservices/rest/RecursoHumanoResource.java
+++ MedCEP/src/org/medcep/webservices/rest/RecursoHumanoResource.java
@@ -1,24 +1,25 @@
 /*
-    MedCEP - A powerful tool for measure
-    
-    Copyright (C) 2013 Ciro Xavier Maretto
-    Copyright (C) 2015 Henrique Néspoli Castro, Vinícius Soares Fonseca                          
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/lgpl.html>.    
+ * MedCEP - A powerful tool for measure
+ * 
+ * Copyright (C) 2013 Ciro Xavier Maretto
+ * Copyright (C) 2015 Henrique Néspoli Castro, Vinícius Soares Fonseca
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl.html>.
  */
 package org.medcep.webservices.rest;
 
+import java.net.*;
 import java.util.*;
 
 import javax.persistence.*;
@@ -26,85 +27,166 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import javax.ws.rs.core.Response.Status;
 
+import org.hibernate.exception.*;
 import org.medcep.model.organizacao.*;
 import org.openxava.jpa.*;
 
 /**
  * API REST para Recurso Humano.
+ * 
  * @author Vinicius
  *
  */
 @Path("RecursoHumano")
-public class RecursoHumanoResource {
+public class RecursoHumanoResource
+{
 
-	@GET
-	@Path("/{id}")
-	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public RecursoHumano get(@PathParam("id") String id) {
-		return XPersistence.getManager().find(RecursoHumano.class, id);
-	}
+    @Context
+    private UriInfo uriInfo;
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response get(@PathParam("id") String id)
+    {
+
+	Response response;
+
+	 if (!XPersistence.getManager().getTransaction().isActive())
+		XPersistence.getManager().getTransaction().begin();	
 	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public List<RecursoHumano> getAll()
-	{
-		TypedQuery<RecursoHumano> query = XPersistence.getManager().createQuery("FROM RecursoHumano", RecursoHumano.class);
-	    return (List<RecursoHumano>) query.getResultList();		
-	}
+	if (id == null || id.isEmpty())
+	    response = Response.status(Status.BAD_REQUEST).build();
+
+	RecursoHumano recursoHumano = XPersistence.getManager().find(RecursoHumano.class, id);
+
+	if (recursoHumano == null)
+	    response = Response.status(Status.NOT_FOUND).build();
+	else
+	    response = Response.status(Status.OK).entity(recursoHumano).build();
+
+	return response;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response getAll()
+    {
+	Response response;
 	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public RecursoHumano post(RecursoHumano recursoHumano) throws Exception
-	{
-		try
-		{
-			XPersistence.getManager().persist(recursoHumano);
-			XPersistence.getManager().getTransaction().commit();
-			return recursoHumano;	
-		}
-		catch (Exception ex)
-		{
-			XPersistence.getManager().getTransaction().rollback();
-			throw ex;
-		}
-	}
+	 if (!XPersistence.getManager().getTransaction().isActive())
+		XPersistence.getManager().getTransaction().begin();
 	
-	@PUT
-	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")	
-	public RecursoHumano put(RecursoHumano recursoHumano) throws Exception
+	TypedQuery<RecursoHumano> query = XPersistence.getManager().createQuery("FROM RecursoHumano", RecursoHumano.class);
+	List<RecursoHumano> result = query.getResultList();
+
+	if (result == null)
+	    response = Response.status(Status.NOT_FOUND).build();
+	else
+	    response = Response.status(Status.OK).entity(result).build();
+
+	return response;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response post(RecursoHumano recursoHumano) throws Exception
+    {
+	try
 	{
-		try
-		{
-			XPersistence.getManager().merge(recursoHumano);
-			XPersistence.getManager().getTransaction().commit();
-			return recursoHumano;	
-		}
-		catch (Exception ex)
-		{
-			XPersistence.getManager().getTransaction().rollback();
-			throw ex;
-		}
+	    Response response;
+
+	    if (!XPersistence.getManager().getTransaction().isActive())
+		XPersistence.getManager().getTransaction().begin();
+		
+	    if (recursoHumano == null || recursoHumano.getId() != null)
+	    {
+		response = Response.status(Status.BAD_REQUEST).build();
+	    }
+	    else if (XPersistence.getManager().contains(recursoHumano))
+	    {
+		response = Response.status(Status.CONFLICT).build();
+	    }
+	    else
+	    {
+		XPersistence.getManager().persist(recursoHumano);
+		XPersistence.getManager().getTransaction().commit();
+		URI location = new URI(String.format("%s/%s", uriInfo.getAbsolutePath().toString(), recursoHumano.getId()));
+		response = Response.created(location).entity(recursoHumano).build();
+	    }
+
+	    return response;
 	}
-	
-	@DELETE
-	@Path("/{id}")
-	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")	
-	public Response delete(@PathParam("id") String id) throws Exception
+	catch (Exception ex)
 	{
-		try
-		{
-			RecursoHumano recursoHumano = XPersistence.getManager().find(RecursoHumano.class, id);
-			XPersistence.getManager().remove(recursoHumano);
-			XPersistence.getManager().getTransaction().commit();
-			 return Response.status(Status.NO_CONTENT).build();
-		}
-		catch (Exception ex)
-		{
-			XPersistence.getManager().getTransaction().rollback();
-			throw ex;
-		}
+	    if (ex.getCause() != null && 
+		    ex.getCause().getCause() != null && 
+		    ex.getCause().getCause() instanceof ConstraintViolationException)
+		return Response.status(Status.CONFLICT).build();
+
+	    throw ex;
 	}
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response put(RecursoHumano recursoHumano) throws Exception
+    {
+	try
+	{
+	    Response response;
+	    
+	    if (!XPersistence.getManager().getTransaction().isActive())
+		XPersistence.getManager().getTransaction().begin();
+	    
+	    if (recursoHumano == null || recursoHumano.getId() == null || recursoHumano.getId().isEmpty())
+		response = Response.status(Status.BAD_REQUEST).build();
+	    else
+	    {
+		XPersistence.getManager().merge(recursoHumano);
+		XPersistence.getManager().getTransaction().commit();
+		response = Response.status(Status.OK).build();
+	    }
+
+	    return response;
+	}
+	catch (Exception ex)
+	{
+	    throw ex;
+	}
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response delete(@PathParam("id") String id) throws Exception
+    {
+	try
+	{
+	    Response response;
+	    
+	    if (!XPersistence.getManager().getTransaction().isActive())
+		XPersistence.getManager().getTransaction().begin();
+	    
+	    RecursoHumano recursoHumano = XPersistence.getManager().find(RecursoHumano.class, id);
+
+	    if (recursoHumano == null)
+		response = Response.status(Status.OK).build();
+	    else
+	    {
+		XPersistence.getManager().remove(recursoHumano);
+		XPersistence.getManager().getTransaction().commit();
+		response = Response.status(Status.OK).build();
+	    }
+
+	    return response;
+	}
+	catch (Exception ex)
+	{
+	    throw ex;
+	}
+    }
 }
