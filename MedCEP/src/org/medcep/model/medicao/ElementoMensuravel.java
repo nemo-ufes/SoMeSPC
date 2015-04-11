25ef61e55bbfa1b37fc0e6d3825bb52fbf5ac488
Vinícius Fonseca <vinnysoft23@gmail.com>
2015-04-11 03:21:20 -0300

    Alteração do modelo para permitir que o mesmo elemento mensurável seja cadastrado como Diretamente e Indiretamente Mensurável, visto que depende do contexto da medição.

--- MedCEP/src/org/medcep/model/medicao/ElementoMensuravel.java
+++ MedCEP/src/org/medcep/model/medicao/ElementoMensuravel.java
@@ -1,21 +1,21 @@
 /*
-    MedCEP - A powerful tool for measure
-    
-    Copyright (C) 2013 Ciro Xavier Maretto
-    Copyright (C) 2015 Henrique Néspoli Castro, Vinícius Soares Fonseca                          
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/lgpl.html>.    
+ * MedCEP - A powerful tool for measure
+ * 
+ * Copyright (C) 2013 Ciro Xavier Maretto
+ * Copyright (C) 2015 Henrique Néspoli Castro, Vinícius Soares Fonseca
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl.html>.
  */
 package org.medcep.model.medicao;
 
@@ -23,135 +23,153 @@ import java.util.*;
 
 import javax.persistence.*;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.*;
 import org.openxava.annotations.*;
 
 @Entity
 @Views({
-	@View(members="nome; tipoElementoMensuravel; descricao"),
-	@View(name="Simple", members="nome"),
-	@View(name="SimpleNoFrame", members="nome")
-	})
-@Tabs({
-	@Tab(properties="nome", defaultOrder="${nome} asc")
+	@View(members = "nome; tipoElementoMensuravel; descricao"),
+	@View(name = "Simple", members = "nome"),
+	@View(name = "SimpleNoFrame", members = "nome")
 })
-public class ElementoMensuravel {
- 
-	@Id @GeneratedValue(generator="system-uuid") @Hidden
-	@GenericGenerator(name="system-uuid", strategy = "uuid")
-    private String id;   
-    
-    @Column(length=500, unique=true) @Required
-	private String nome;
-	
-	@Stereotype("TEXT_AREA")
-	@Column(columnDefinition="TEXT")
-	private String descricao;
-	
-	@ManyToOne 
-	@DescriptionsList(descriptionProperties="nome")
-	@NoCreate
-	@NoModify
-	@Required
-	private TipoElementoMensuravel tipoElementoMensuravel;
+@Tabs({
+	@Tab(properties = "nome", defaultOrder = "${nome} asc")
+})
+@Table(uniqueConstraints =
+	@UniqueConstraint(columnNames = { "nome", "tipoelementomensuravel_id" }))
+public class ElementoMensuravel
+{
 
-	public String getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @Hidden
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    @Column(length = 500)
+    @Required
+    private String nome;
 
-	public String getNome() {
-		return nome;
-	}
+    @Stereotype("TEXT_AREA")
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    @ManyToOne
+    @DescriptionsList(descriptionProperties = "nome")
+    @NoCreate
+    @NoModify
+    @Required
+    private TipoElementoMensuravel tipoElementoMensuravel;
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public String getId()
+    {
+	return id;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    public void setId(String id)
+    {
+	this.id = id;
+    }
 
-	public TipoElementoMensuravel getTipoElementoMensuravel() {
-		return tipoElementoMensuravel;
-	}
+    public String getNome()
+    {
+	return nome;
+    }
 
-	public void setTipoElementoMensuravel(TipoElementoMensuravel tipoElementoMensuravel) {
-		this.tipoElementoMensuravel = tipoElementoMensuravel;
-	}
+    public void setNome(String nome)
+    {
+	this.nome = nome;
+    }
 
-	public Collection<Medida> getMedida() {
-		return medida;
-	}
+    public String getDescricao()
+    {
+	return descricao;
+    }
 
-	public void setMedida(Collection<Medida> medida) {
-		this.medida = medida;
-	}
+    public void setDescricao(String descricao)
+    {
+	this.descricao = descricao;
+    }
 
-	public Collection<ElementoMensuravel> getSubelemento() {
-		return subelemento;
-	}
+    public TipoElementoMensuravel getTipoElementoMensuravel()
+    {
+	return tipoElementoMensuravel;
+    }
 
-	public void setSubelemento(Collection<ElementoMensuravel> subelemento) {
-		this.subelemento = subelemento;
-	}
+    public void setTipoElementoMensuravel(TipoElementoMensuravel tipoElementoMensuravel)
+    {
+	this.tipoElementoMensuravel = tipoElementoMensuravel;
+    }
 
-	@OneToMany(mappedBy="elementoMensuravel")
-	private Collection<Medida> medida;
+    public Collection<Medida> getMedida()
+    {
+	return medida;
+    }
+
+    public void setMedida(Collection<Medida> medida)
+    {
+	this.medida = medida;
+    }
+
+    public Collection<ElementoMensuravel> getSubelemento()
+    {
+	return subelemento;
+    }
+
+    public void setSubelemento(Collection<ElementoMensuravel> subelemento)
+    {
+	this.subelemento = subelemento;
+    }
+
+    @OneToMany(mappedBy = "elementoMensuravel")
+    private Collection<Medida> medida;
 
     @ManyToMany
     @JoinTable(
-	      name="subelemento"
-	      , joinColumns={
-	    		  @JoinColumn(name="subelemento_id")
-	       }
-	      , inverseJoinColumns={
-	    		  @JoinColumn(name="subelemento_id2")
-	       }
-	      )
-	private Collection<ElementoMensuravel> subelemento;
-	 
+	    name = "subelemento"
+	    , joinColumns = {
+		    @JoinColumn(name = "subelemento_id")
+	    }
+	    , inverseJoinColumns = {
+		    @JoinColumn(name = "subelemento_id2")
+	    })
+    private Collection<ElementoMensuravel> subelemento;
+
     @ManyToMany
     @JoinTable(
-	      name="elementoMensuravel_tipoDeEntidadeMensuravel"
-	      , joinColumns={
-	    		  @JoinColumn(name="elementoMensuravel_id")
-	       }
-	      , inverseJoinColumns={
-	    		  @JoinColumn(name="tipoDeEntidadeMensuravel_id")
-	       }
-	      )
-	private Collection<TipoDeEntidadeMensuravel> tipoDeEntidadeMensuravel;
+	    name = "elementoMensuravel_tipoDeEntidadeMensuravel"
+	    , joinColumns = {
+		    @JoinColumn(name = "elementoMensuravel_id")
+	    }
+	    , inverseJoinColumns = {
+		    @JoinColumn(name = "tipoDeEntidadeMensuravel_id")
+	    })
+    private Collection<TipoDeEntidadeMensuravel> tipoDeEntidadeMensuravel;
 
+    @OneToMany(mappedBy = "elementoMensuravel")
+    private Collection<Medicao> medicao;
 
-	@OneToMany(mappedBy="elementoMensuravel")
-	private Collection<Medicao> medicao;
+    public Collection<Medicao> getMedicao()
+    {
+	return medicao;
+    }
 
-	public Collection<Medicao> getMedicao() {
-		return medicao;
-	}
+    public void setMedicao(Collection<Medicao> medicao)
+    {
+	this.medicao = medicao;
+    }
 
-	public void setMedicao(Collection<Medicao> medicao) {
-		this.medicao = medicao;
-	}	
-	
+    public Collection<TipoDeEntidadeMensuravel> getTipoDeEntidadeMensuravel()
+    {
+	return tipoDeEntidadeMensuravel;
+    }
 
-	public Collection<TipoDeEntidadeMensuravel> getTipoDeEntidadeMensuravel() {
-		return tipoDeEntidadeMensuravel;
-	}
+    public void setTipoDeEntidadeMensuravel(
+	    Collection<TipoDeEntidadeMensuravel> tipoDeEntidadeMensuravel)
+    {
+	this.tipoDeEntidadeMensuravel = tipoDeEntidadeMensuravel;
+    }
 
-	public void setTipoDeEntidadeMensuravel(
-			Collection<TipoDeEntidadeMensuravel> tipoDeEntidadeMensuravel) {
-		this.tipoDeEntidadeMensuravel = tipoDeEntidadeMensuravel;
-	}
-	
 }
- 
