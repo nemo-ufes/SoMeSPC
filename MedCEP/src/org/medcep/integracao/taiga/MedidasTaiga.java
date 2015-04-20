c5fc59de7215013295b0dd861e2f1bec062f535a
Vinícius Fonseca <vinnysoft23@gmail.com>
2015-04-19 22:58:35 -0300

    Descrição das medidas no enum.

--- MedCEP/src/org/medcep/integracao/taiga/MedidasTaiga.java
+++ MedCEP/src/org/medcep/integracao/taiga/MedidasTaiga.java
@@ -22,30 +22,44 @@ package org.medcep.integracao.taiga;
 public enum MedidasTaiga
 {
     //Medidas do Projeto
-    PONTOS_ALOCADOS_PROJETO,    
-    PONTOS_DEFINIDOS_PROJETO,    
-    PONTOS_FECHADOS_PROJETO,    
-    TOTAL_SPRINTS_PROJETO,
-    TOTAL_PONTOS_PROJETO,
-    VELOCIDADE_PROJETO,
+    PONTOS_ALOCADOS_PROJETO("Pontos Alocados no Projeto"),    
+    PONTOS_DEFINIDOS_PROJETO("Pontos Definidos no Projeto"),    
+    PONTOS_FECHADOS_PROJETO("Pontos Fechados no Projeto"),    
+    TOTAL_SPRINTS_PROJETO("Total de Sprints do Projeto"),
+    TOTAL_PONTOS_PROJETO("Total de Pontos do Projeto"),
+    VELOCIDADE_PROJETO("Velocidade do Projeto"),
 
     //Medidas do Recurso Humano na Equipe (Mede a Alocação)
-    PONTOS_ALOCADOS_POR_PAPEL_PROJETO,
-    PONTOS_DEFINIDOS_POR_PAPEL_PROJETO,
-    PONTOS_FECHADOS_POR_PAPEL_PROJETO,
+    PONTOS_ALOCADOS_POR_PAPEL_PROJETO("Pontos Alocados por Papel no Projeto"),
+    PONTOS_DEFINIDOS_POR_PAPEL_PROJETO("Pontos Definidos por Papel no Projeto"),
+    PONTOS_FECHADOS_POR_PAPEL_PROJETO("Pontos Fechados por Papel no Projeto"),
     
     //Medidas da Sprint
-    DOSES_IOCAINE_SPRINT,
-    DURACAO_SPRINT, //Data Fim - Data Inicio
-    ESTORIAS_COMPLETADAS_SPRINT,
-    PONTOS_COMPLETADOS_SPRINT,
-    TAREFAS_COMPLETADAS_SPRINT,
-    TOTAL_ESTORIAS_SPRINT,
-    TOTAL_PONTOS_SPRINT,
-    TOTAL_TAREFAS_SPRINT,
+    DOSES_IOCAINE_SPRINT("Doses de Iocaine da Sprint"),
+    DURACAO_SPRINT("Duração da Sprint"), //Data Fim - Data Inicio
+    ESTORIAS_COMPLETADAS_SPRINT("Estórias Completadas na Sprint"),
+    PONTOS_COMPLETADOS_SPRINT("Pontos Completados na Sprint"),
+    TAREFAS_COMPLETADAS_SPRINT("Tarefas Completadas na Sprint"),
+    TOTAL_ESTORIAS_SPRINT("Total de Estórias da Sprint"),
+    TOTAL_PONTOS_SPRINT("Total de Pontos da Sprint"),
+    TOTAL_TAREFAS_SPRINT("Total de Tarefas da Sprint"),
         
     //Medidas da Estória
-    DURACAO_ESTORIA, //Data Fim - Data Inicio
-    PONTOS_ESTORIA
+    DURACAO_ESTORIA("Duração da Estória"), //Data Fim - Data Inicio
+    PONTOS_ESTORIA("Pontos da Estória");
+    
+    private final String name;       
+
+    private MedidasTaiga(String s) {
+        name = s;
+    }
+
+    public boolean equalsName(String otherName){
+        return (otherName == null)? false:name.equals(otherName);
+    }
+
+    public String toString(){
+       return name;
+    }
     
 };
