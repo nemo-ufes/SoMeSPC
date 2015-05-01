231fe22ff3a076f3b0b7ec1947b2196ca2fc077f
Vinícius Fonseca <vinnysoft23@gmail.com>
2015-05-01 16:18:31 -0300

    Melhorias no layout.

--- Addons/src/com/openxava/naviox/web/NaviOXServlet.java
+++ Addons/src/com/openxava/naviox/web/NaviOXServlet.java
@@ -1,15 +1,12 @@
 package com.openxava.naviox.web;
 
 import java.io.*;
-import java.util.*;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 import org.openxava.util.*;
 import org.openxava.web.style.*;
-
-import com.openxava.phone.web.*;
 
 /**
  * 
@@ -23,7 +20,7 @@ public class NaviOXServlet extends HttpServlet {
 			response.getWriter().print(XavaResources.getString(request, "module_name_missing"));
 			return;
 		}
-		String url = Browsers.isMobile(request)?"/p/" + uri[3]:"/naviox/index.jsp?application=" + uri[1] + "&module=" + uri[3];
+		String url = "/naviox/index.jsp?application=" + uri[1] + "&module=" + uri[3];
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);		
 		
 		Style.setPotalInstance(NaviOXStyle.getInstance()); // We manage style in NaviOX as in the portal case, to override the style defined in xava.properties and by device 
