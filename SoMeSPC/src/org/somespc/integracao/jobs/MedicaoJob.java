37cad20fb7a818f179fd42b96c27e5b6b2053d1d
vinnysoft <vinnysoft23@gmail.com>
2015-11-02 13:31:04 -0200

    Remoção de propriedades não usadas.

--- SoMeSPC/src/org/somespc/integracao/jobs/MedicaoJob.java
+++ SoMeSPC/src/org/somespc/integracao/jobs/MedicaoJob.java
@@ -3,13 +3,8 @@ package org.somespc.integracao.jobs;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.somespc.model.definicao_operacional_de_medida.Periodicidade;
-import org.somespc.model.plano_de_medicao.ItemPlanoMedicao;
 
 public abstract class MedicaoJob implements Job {
-
-	private ItemPlanoMedicao itemParaMedir;
-	private Periodicidade periodicidade;
 
 	@Override
 	public synchronized void execute(JobExecutionContext arg0) throws JobExecutionException {
@@ -17,21 +12,5 @@ public abstract class MedicaoJob implements Job {
 	}
 
 	public abstract void executarMedicao(JobExecutionContext context) throws JobExecutionException;
-
-	public ItemPlanoMedicao getItemParaMedir() {
-		return itemParaMedir;
-	}
-
-	public void setItemParaMedir(ItemPlanoMedicao itemParaMedir) {
-		this.itemParaMedir = itemParaMedir;
-	}
-
-	public Periodicidade getPeriodicidade() {
-		return periodicidade;
-	}
-
-	public void setPeriodicidade(Periodicidade periodicidade) {
-		this.periodicidade = periodicidade;
-	}
 
 }
