9ccab7e7b10052c7ff19007703809d4cffa69a07
vinnysoft <vinnysoft23@gmail.com>
2015-11-23 22:11:06 -0200

    Teste de merge.

--- SoMeSPC/src/org/somespc/integracao/sonarqube/SonarQubeIntegrator.java
+++ SoMeSPC/src/org/somespc/integracao/sonarqube/SonarQubeIntegrator.java
@@ -365,9 +365,8 @@ public class SonarQubeIntegrator {
 
 	public synchronized void agendarSonarQubeMedicaoJob(PlanoDeMedicaoDoProjeto plano, String nomeProjeto, String chaveRecurso, 
 			ItemPlanoMedicao item, Periodicidade periodicidade, SonarLoginDTO login) throws Exception {
-
 		
-		// Inicia o scheduler.
+		// Inicia o scheduler
 		SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
 		Scheduler sched = schedFact.getScheduler();
 
